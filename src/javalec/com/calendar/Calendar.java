f1df838f1f6c9a090fd2076da49290496d7f7604
skeri <skeri@DESKTOP-COU2UJ4>
2017-10-16 22:25:26 +0900

    Leap Year Question

--- src/javalec/com/calendar/Calendar.java
+++ src/javalec/com/calendar/Calendar.java
@@ -15,12 +15,26 @@ public class Calendar {
 	}
 	
 	private static final int[] MAX_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
+	private static final int[] LEAP_MAX_DAYS = {31,29,31,30,31,30,31,31,30,31,30,31};
 	
-	public int getMaxDaysOfMonth(int month) {
+	public int getMaxDaysOfMonth(int year,int month) {
 		// TODO Auto-generated method stub
 		
-		return MAX_DAYS[month - 1];
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		}else {
+			return MAX_DAYS[month - 1];
+		}
+	}
 	
+	public boolean isLeapYear(int year) {
+		// TODO Auto-generated method stub
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		}	
+		else {
+			return false;
+		}
 	}
 		
 	public void printCalendar(int month) {
@@ -28,6 +42,7 @@ public class Calendar {
 		System.out.println(" 일\t월\t화\t수\t목\t금\t토");
 		System.out.println("--------------------------------------------------");
 		
+			
 		for (int i = 1; i < month + 1; i++) {
 			if (i % 8 == 0) {
 				System.out.println("");
@@ -46,12 +61,14 @@ public class Calendar {
 		Scanner scanner = new Scanner(System.in);
 //		String s1 = scanner.next();
 		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
 //		int a = Integer.parseInt(s1);
 
 		Calendar cal = new Calendar();
 		
-		System.out.printf("입력하신 %d월은 %d일 입니다.\n",a,cal.getMaxDaysOfMonth(a));
-		cal.printCalendar(cal.getMaxDaysOfMonth(a));
+		System.out.printf("입력하신 %d년 %d월은 %d일 입니다.\n",a,b,cal.getMaxDaysOfMonth(a,b));
+		cal.printCalendar(cal.getMaxDaysOfMonth(a, b));
 		scanner.close();
 
 		
